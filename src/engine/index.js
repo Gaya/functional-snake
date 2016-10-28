14b35705e1bf7e5d984c53040efdd17f02384708
Gaya Kessler <gaya.kessler@gmail.com>
2016-10-28 10:17:05 +0200

    setup game basics

---
+++ src/engine/index.js
@@ -0,0 +1,11 @@
+export function setup() {
+
+}
+
+export function update() {
+
+}
+
+export function draw() {
+
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import createCanvas from 'canvas';
+import { setup, update, draw } from 'engine';
 
+let state = {};
 const canvas = createCanvas();
+
+// get initial state after setup
+state = setup({ state, canvas });
+
+window.requestAnimationFrame((timestamp) => {
+  // calculate new state
+  state = update({ timestamp, state });
+
+  // draw canvas from state
+  draw({ state, canvas });
+});
