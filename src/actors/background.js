c0a1b94cc8b93bc8c724caf03cb67398f3d94d02
Gaya Kessler <gaya.kessler@gmail.com>
2016-11-07 16:35:39 +0100

    paint screen red if dead

--- src/actors/background.js
+++ src/actors/background.js
@@ -5,6 +5,6 @@ export function update() {
 export function draw({ state = { game: { width: 60, height: 60 } }, canvas = null }) {
   const ctx = canvas.getContext();
 
-  ctx.fillStyle = 'black';
+  ctx.fillStyle = state.snake.dead ? 'red' : 'black';
   ctx.fillRect(0, 0, state.game.width * state.game.scale, state.game.height * state.game.scale);
 }
