b49d5f1132998303a98378c58e48ddc4a17dd626
Gaya Kessler <gaya.kessler@gmail.com>
2016-11-10 07:35:00 +0100

    never remember the state

--- src/main.js
+++ src/main.js
@@ -2,30 +2,30 @@ import createCanvas from 'canvas';
 import { setup, update, draw } from 'engine';
 import { GAME_WIDTH, GAME_HEIGHT, GAME_SCALE } from 'constants/game';
 
-let state = {};
 const canvas = createCanvas(GAME_WIDTH * GAME_SCALE, GAME_HEIGHT * GAME_SCALE);
 
-function setState(newState) {
-  state = newState;
-}
+// get initial state after setup
+const initialState = setup({});
 
 // set focus to window
 window.focus();
 
-// get initial state after setup
-setState(setup({ state }));
-
-function tick(timestamp) {
-  // calculate new state
-  setState({
-    ...state,
-    ...update({ timestamp, state }),
-  });
-
-  // draw canvas from state
-  draw({ timestamp, state, canvas });
-
-  window.requestAnimationFrame(tick);
+function injectStateToTick(state, cb) {
+  return timestamp => cb(timestamp, state);
 }
 
-tick();
+function tick(timestamp, state) {
+  // calculate new state
+  const newState = {
+    ...state,
+    ...update({ timestamp, state }),
+  };
+
+  // draw canvas from state
+  draw({ timestamp, newState, canvas });
+
+  window.requestAnimationFrame(injectStateToTick(newState, tick));
+}
+
+// run program
+tick(0, initialState);
